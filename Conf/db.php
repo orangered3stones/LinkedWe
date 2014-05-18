8e3b624c1b5dbeefe0180ac099008194b88f7542
hongleic <orangered3stones@gmail.com>
2014-05-18 20:19:11 +0800

    submit trank code
    hongleic

---
+++ Conf/db.php
@@ -0,0 +1,18 @@
+<?php
+return array(
+	
+'DB_TYPE'=>'mysql',
+	
+'DB_HOST'=>'localhost',
+	
+'DB_PORT'=>'3306',
+	
+'DB_NAME'=>'weixinxb',
+	
+'DB_USER'=>'weixinxb',
+	
+'DB_PWD'=>'weixinxb',
+	
+'DB_PREFIX'=>'tp_',
+
+);
